a872cd9955a9c9cd17a6749d29ec8984980ffe0c
hwimli-mouheb <mohamedmouhebhouimli@insat.u-carthage.tn>
2023-02-09 23:00:57 +0100

    DIP

---
+++ DIP/src/com/directi/training/dip/exercice_refactored/EncodingInterface.java
@@ -0,0 +1,8 @@
+package com.directi.training.dip.exercice_refactored;
+
+import java.io.IOException;
+
+public interface EncodingInterface {
+    public void encodeWithFiles() throws IOException;
+    public void encodeBasedOnNetworkAndDatabase() throws IOException;
+}
